9e60de61c14e836ed17ff30271dd993bb82686e3
Nathan Do <nathando@Nathans-MacBook-Air-3.local>
2025-06-06 16:47:35 -0700

    Added ESLint and Prettier, Created TabBarIcons

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,12 +1,21 @@
-import { Tabs } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Tabs } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesome } from '@expo/vector-icons';
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />;
+}
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
-        <Tabs screenOptions={{
-          tabBarActiveTintColor: "#1BC464",
-          tabBarInactiveTintColor: "gray",
+    <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#1BC464',
+          tabBarInactiveTintColor: 'gray',
           tabBarLabelStyle: { fontSize: 16 },
           tabBarStyle: {
             borderRadius: 50,
@@ -14,17 +23,33 @@ const TabsLayout = () => {
             marginHorizontal: 20,
             marginBottom: 30,
             height: 70,
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            position: "absolute",
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            position: 'absolute',
           },
           headerShown: false,
         }}
-        >
-            <Tabs.Screen name="index" options={{ headerShown: false }} />
-            <Tabs.Screen name="orders" options={{ headerShown: false }} />
-        </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Shop',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="shopping-cart" />;
+            },
+          }}
+        />
+        <Tabs.Screen
+          name="orders"
+          options={{
+            title: 'Orders',
+            tabBarIcon(props) {
+              return <TabBarIcon {...props} name="book" />;
+            },
+          }}
+        />
+      </Tabs>
     </SafeAreaView>
   );
 };
