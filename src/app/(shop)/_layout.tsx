f1c261861d036b0fb1b09ed0bff154e5c51b1649
Nathan Do <nathando@MacBookAir.lan>
2025-06-05 18:12:02 -0700

    Added custom Tab Bar to bottom

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -3,8 +3,25 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const TabsLayout = () => {
   return (
-    <SafeAreaView>
-        <Tabs>
+    <SafeAreaView style={{ flex: 1 }} edges={["top"]}>
+        <Tabs screenOptions={{
+          tabBarActiveTintColor: "#1BC464",
+          tabBarInactiveTintColor: "gray",
+          tabBarLabelStyle: { fontSize: 16 },
+          tabBarStyle: {
+            borderRadius: 50,
+            paddingTop: 5,
+            marginHorizontal: 20,
+            marginBottom: 30,
+            height: 70,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            position: "absolute",
+          },
+          headerShown: false,
+        }}
+        >
             <Tabs.Screen name="index" options={{ headerShown: false }} />
             <Tabs.Screen name="orders" options={{ headerShown: false }} />
         </Tabs>
