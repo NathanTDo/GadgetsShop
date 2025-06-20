be0239dcddfc0f6748841696701bbc50517f1c4c
Nathan Do <nathantdo04@gmail.com>
2025-06-20 08:40:31 -0700

    Added Product Detail Pages (slug) and Cart Update

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,28 +1,31 @@
 import { View, Text } from 'react-native';
 import { Stack } from 'expo-router';
 import '../../global.css';
+import { ToastProvider } from 'react-native-toast-notifications';
 
 const RootLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(shop)"
-        options={{ headerShown: false, title: 'Shop' }}
-      />
-      <Stack.Screen
-        name="product"
-        options={{ headerShown: true, title: 'product' }}
-      />
-      <Stack.Screen
-        name="categories"
-        options={{ headerShown: false, title: 'categories' }}
-      />
-      <Stack.Screen
-        name="cart"
-        options={{ presentation: 'modal', title: 'Shopping Cart' }}
-      />
-      <Stack.Screen name="auth" options={{ headerShown: true }} />
-    </Stack>
+    <ToastProvider>
+      <Stack>
+        <Stack.Screen
+          name="(shop)"
+          options={{ headerShown: false, title: 'Shop' }}
+        />
+        <Stack.Screen
+          name="product"
+          options={{ headerShown: false, title: 'product' }}
+        />
+        <Stack.Screen
+          name="categories"
+          options={{ headerShown: false, title: 'categories' }}
+        />
+        <Stack.Screen
+          name="cart"
+          options={{ presentation: 'modal', title: 'Shopping Cart' }}
+        />
+        <Stack.Screen name="auth" options={{ headerShown: true }} />
+      </Stack>
+    </ToastProvider>
   );
 };
 
--- src/app/categories/_layout.tsx
+++ src/app/categories/_layout.tsx
@@ -1,6 +1,6 @@
-import { Stack, useRouter } from "expo-router";
-import { TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const CategoryLayout = () => {
   const router = useRouter();
