71b24eacbd0358e9a7d8e7756a8c9ced4817ae65
Nathan Do <nathantdo04@gmail.com>
2025-06-20 20:13:18 -0700

    Finished Orders Page

--- src/app/(shop)/orders/_layout.tsx
+++ src/app/(shop)/orders/_layout.tsx
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
-import { Stack } from "expo-router";
+import { View, Text } from 'react-native';
+import { Stack } from 'expo-router';
 
 const OrdersLayout = () => {
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerTitle: 'Orders',
+          headerStyle: {
+            backgroundColor: '#fff',
+          },
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default OrdersLayout;
