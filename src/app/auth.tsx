1e8eea9e1decf78634a7f107e44baf2ffcd717ad
Nathan Do <nathantdo04@gmail.com>
2025-06-21 23:57:55 -0700

    Adding Auth Page

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,11 +1,137 @@
-import { View, Text} from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+} from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const authSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long' }),
+});
 
 const Auth = () => {
+  const { control, handleSubmit, formState } = useForm({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const signIn = (data: z.infer<typeof authSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <ImageBackground
+      source={{
+        uri: 'https://images.pexels.com/photos/682933/pexels-photo-682933.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      }}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.overlay} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.subtitle}>Please authenticate to continue.</Text>
+
+        <Controller
+          control={control}
+          name="email"
+          render={({
+            field: { onChange, onBlur, value },
+            fieldState: { error },
+          }) => (
+              <TextInput
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                style={styles.input}
+                placeholder="Email"
+                placeholderTextColor="#AAA"
+                autoCapitalize="none"
+                editable={!formState.isSubmitting}
+              />
+              {error && <Text style={styles.error}>{error.message}</Text>}
+          )}
+        />
+      </View>
+    </ImageBackground>
   );
 };
 
 export default Auth;
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    width: '100%',
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#ddd',
+    marginBottom: 32,
+  },
+  input: {
+    width: '90%',
+    padding: 12,
+    marginBottom: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 8,
+    fontSize: 16,
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#6a1b9a',
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    width: '90%',
+    alignItems: 'center',
+  },
+  signUpButton: {
+    backgroundColor: 'transparent',
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+  signUpButtonText: {
+    color: '#fff',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  error: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 16,
+    textAlign: 'left',
+    width: '90%',
+  },
+});
